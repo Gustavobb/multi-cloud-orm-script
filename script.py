9571889f09bb0f74c3cb3583ef23748817915aab
Gustavo <gustavobb@al.insper.edu.br>
2020-11-14 18:30:23 -0300

    loadbalancer not done

--- script.py
+++ script.py
@@ -3,100 +3,121 @@ from botocore.exceptions import ClientError
 
 class AwsConnection:
 
-    def __init__(self, region, security_group_tag, instance_tag, key_tag):
-        self.ec2_resource = boto3.resource('ec2', region_name = region)
-        self.ec2_client = boto3.client('ec2', region_name = region)
-        self.security_group_tag = security_group_tag
-        self.instance_tag = instance_tag
-        self.key_tag = key_tag
-        self.key_name = None
-        self.security_group_id = None
+     def __init__(self, region, security_group_tag, instance_tag, key_tag):
+          self.ec2_resource = boto3.resource('ec2', region_name = region)
+          self.ec2_client = boto3.client('ec2', region_name = region)
+          self.security_group_tag = security_group_tag
+          self.instance_tag = instance_tag
+          self.key_tag = key_tag
+          self.key_name = None
+          self.security_group_id = None
+          self.vpc_id = self.ec2_client.describe_vpcs().get('Vpcs', [{}])[0].get('VpcId', '')
 
-    def create_instance(self, image_id, user_data):
-        print("\nCreating instance, image_id=%s, key_name=%s" % (image_id, self.key_name))
-        try:
-            waiter = self.ec2_client.get_waiter('instance_status_ok')
-            # create a new EC2 instance
-            instance = self.ec2_resource.create_instances(
-                 ImageId=image_id,
-                 MinCount=1,
-                 MaxCount=1,
-                 InstanceType='t2.micro',
-                 SecurityGroupIds=[self.security_group_id],
-                 UserData=user_data,
-                 KeyName=self.key_name,
-                 TagSpecifications=[{'ResourceType': 'instance', 'Tags': [self.instance_tag]}]
-            )
+     def create_instance(self, image_id, user_data):
+          print("\nCreating instance, image_id=%s, key_name=%s" % (image_id, self.key_name))
+          try:
+               waiter = self.ec2_client.get_waiter('instance_status_ok')
+               # create a new EC2 instance
+               instance = self.ec2_resource.create_instances(
+                    ImageId=image_id,
+                    MinCount=1,
+                    MaxCount=1,
+                    InstanceType='t2.micro',
+                    SecurityGroupIds=[self.security_group_id],
+                    UserData=user_data,
+                    KeyName=self.key_name,
+                    TagSpecifications=[{'ResourceType': 'instance', 'Tags': [self.instance_tag]}]
+               )
 
-            waiter.wait(InstanceIds=[instance[0].id])
-            public_ip = self.ec2_client.describe_instances(InstanceIds=[instance[0].id])['Reservations'][0]['Instances'][0]['NetworkInterfaces'][0]['PrivateIpAddresses'][0]['Association']['PublicIp']
-            print("Instance %s created and checked, public_ip=%s" % (instance[0].id, public_ip))
-            return public_ip, instance[0].id
+               waiter.wait(InstanceIds=[instance[0].id])
+               public_ip = self.ec2_client.describe_instances(InstanceIds=[instance[0].id])['Reservations'][0]['Instances'][0]['NetworkInterfaces'][0]['PrivateIpAddresses'][0]['Association']['PublicIp']
+               print("Instance %s created and checked, public_ip=%s" % (instance[0].id, public_ip))
+               return public_ip, instance[0].id
 
-        except ClientError as e:
-            print('Error', e)
+          except ClientError as e:
+               print('Error', e)
 
-        return
+          return
 
-    def create_key_pair(self, key_name, file):
-        print("\nCreating key %s" % (key_name))
+     def create_key_pair(self, key_name, file):
+          print("\nCreating key %s" % (key_name))
 
-        try:
-            self.key_name = key_name
-            response = self.ec2_client.create_key_pair(
-                 KeyName=key_name,
-                 TagSpecifications=[{'ResourceType': 'key-pair', 'Tags': [self.key_tag]}]
-            )
+          try:
+               self.key_name = key_name
+               response = self.ec2_client.create_key_pair(
+                    KeyName=key_name,
+                    TagSpecifications=[{'ResourceType': 'key-pair', 'Tags': [self.key_tag]}]
+               )
 
-            print("Key created id=%s, name=%s" % (response['KeyPairId'], key_name))
+               print("Key created id=%s, name=%s" % (response['KeyPairId'], key_name))
 
-            with open(file, "w") as f: f.write(response['KeyMaterial'])
+               with open(file, "w") as f: f.write(response['KeyMaterial'])
 
-        except ClientError as e:
-            print('Error', e)
+          except ClientError as e:
+               print('Error', e)
 
-    def create_security_group(self, security_group_name, permissions):
-        print("\nCreating security group %s" % (security_group_name))
-        try:
-            vpc_id = self.ec2_client.describe_vpcs().get('Vpcs', [{}])[0].get('VpcId', '')
-            response = self.ec2_client.create_security_group(
-                 GroupName=security_group_name,
-                 Description='test',
-                 VpcId=vpc_id,
-                 TagSpecifications=[{'ResourceType': 'security-group', 'Tags': [self.security_group_tag]}]
-            )
+     def create_security_group(self, security_group_name, permissions):
+          print("\nCreating security group %s" % (security_group_name))
+          try:
+               response = self.ec2_client.create_security_group(
+                    GroupName=security_group_name,
+                    Description='test',
+                    VpcId=self.vpc_id,
+                    TagSpecifications=[{'ResourceType': 'security-group', 'Tags': [self.security_group_tag]}]
+               )
 
-            self.security_group_id = response['GroupId']
-            print('Security Group Created %s in vpc %s.' % (self.security_group_id, vpc_id))
+               self.security_group_id = response['GroupId']
+               print('Security Group Created %s in vpc %s.' % (self.security_group_id, vpc_id))
 
-            data = self.ec2_client.authorize_security_group_ingress(
-                 GroupId=self.security_group_id,
-                 IpPermissions=permissions
-            )
-            print('Ingress Successfully Set')
+               data = self.ec2_client.authorize_security_group_ingress(
+                    GroupId=self.security_group_id,
+                    IpPermissions=permissions
+               )
+               print('Ingress Successfully Set')
 
-        except ClientError as e:
-            print('Error', e)
+          except ClientError as e:
+               print('Error', e)
 
-    def create_ami(self, instance_id, name):
-        print("\nCreating AMI id=%s, name=%s" % (instance_id, name))
-        try:
-            waiter = self.ec2_client.get_waiter('image_available')
-            response = self.ec2_client.create_image(InstanceId=instance_id, NoReboot=True, Name=name)
-            print(response)
-            waiter.wait(ImageIds=[response[0].id])
-            print("AMI created\n")
+     def create_ami(self, instance_id, name):
+          print("\nCreating AMI id=%s, name=%s" % (instance_id, name))
+          try:
+               waiter = self.ec2_client.get_waiter('image_available')
+               response = self.ec2_client.create_image(InstanceId=instance_id, NoReboot=True, Name=name)
+               print(response)
+               waiter.wait(ImageIds=[response[0].id])
+               print("AMI created\n")
 
-            return response[0].id
+               return response[0].id
 
-        except ClientError as e:
-            print('Error', e)
+          except ClientError as e:
+               print('Error', e)
 
-        return
+          return
 
-    def delete_security_group(self):
-        print("\nDeleting security groups")
-        try:
+     def create_target_groups(self, target_group_name, target_group_tag):
+          response = self.ec2_client.create_target_group(
+               Name=target_group_name,
+               Protocol='HTTP',
+               Port=80,
+               VpcId=self.vpc_id,
+               TargetType='instance',
+               Tags=[target_group_tag]
+          )
+
+          response = self.ec2_client.register_targets(
+               TargetGroupArn='string',
+               Targets=[
+                    {
+                         'Id': 'string',
+                         'Port': 123,
+                         'AvailabilityZone': 'string'
+                    },
+               ]
+          )
+
+     def delete_security_group(self):
+          print("\nDeleting security groups")
+          try:
           security_group_id = self.ec2_client.describe_security_groups(
                Filters=[
                {
@@ -110,12 +131,12 @@ class AwsConnection:
                response = self.ec2_client.delete_security_group(GroupId=security_group_id)
                print('Security Group Deleted %s' % (security_group_id))
 
-        except ClientError as e:
-            print('Error', e)
+          except ClientError as e:
+               print('Error', e)
 
-    def delete_key_pairs(self):
-        print("\nDeleting Key Pairs")
-        try:
+     def delete_key_pairs(self):
+          print("\nDeleting Key Pairs")
+          try:
           key_id = self.ec2_client.describe_key_pairs(
                Filters=[
                {
@@ -124,17 +145,17 @@ class AwsConnection:
                }
           ])
           
-          if len(key_id['KeyPairs']) != 0:
+          if len(key_id['KeyPairs']):
                key_id = key_id['KeyPairs'][0]['KeyPairId']
                response = self.ec2_client.delete_key_pair(KeyPairId=key_id)
                print('Key %s Deleted' % (key_id))
 
-        except ClientError as e:
-            print('Error', e)
+          except ClientError as e:
+               print('Error', e)
 
-    def delete_instances(self):
-        print("\nDeleting Instances")
-        try:
+     def delete_instances(self):
+          print("\nDeleting Instances")
+          try:
 
           instance_id = self.ec2_client.describe_instances(
                Filters=[
@@ -148,19 +169,19 @@ class AwsConnection:
                }
           ])
 
-          if len(instance_id['Reservations']) != 0:
+          if len(instance_id['Reservations']):
                instance_id = instance_id['Reservations'][0]['Instances'][0]['InstanceId']
                waiter = self.ec2_client.get_waiter('instance_terminated')
                response = self.ec2_client.terminate_instances(InstanceIds=[instance_id])
                waiter.wait(InstanceIds=[instance_id])
                print('Instance %s Deleted' % (instance_id))
 
-        except ClientError as e:
-            print('Error', e)
+          except ClientError as e:
+               print('Error', e)
 
-    def delete_image(self, name):
-        response = self.ec2_client.describe_images()
-        print(response)
+     def delete_image(self, name):
+          response = self.ec2_client.describe_images()
+          print(response)
 
 def run_application() -> int:
      # images
@@ -170,10 +191,6 @@ def run_application() -> int:
      # regions
      region_east_1 = "us-east-1"
      region_east_2 = "us-east-2"
-
-     # key names
-     key_name_nv = "gubebra_pf_nv"
-     key_name_oh = "gubebra_pf_oh"
 
      # security groups
      security_group_name_oh = "postgres_instance"
@@ -207,21 +224,44 @@ def run_application() -> int:
      }]
 
      # names
+     # -> ami
      django_ami = "braga_django_ami"
+
+     # -> key
+     key_name_nv = "gubebra_pf_nv"
+     key_name_oh = "gubebra_pf_oh"
+
+     # -> target_group
+     target_group_name_nv = "target_group_braga_pf_nv"
+
+     # -> load balancer
+     load_balancer_name_nv = "load_balancer_braga_pf_nv"
+
+     # -> autoscaling
+     autoscaling_name_nv = "autoscaling_braga_pf_nv"
 
      # tags
      # -> keys
-     key_tag_nv = {'Key': 'Name', 'Value': 'key_braga_pf_nv'}
-     key_tag_oh = {'Key': 'Name', 'Value': 'key_braga_pf_oh'}
+     key_tag_nv = {'Key': 'Name', 'Value': 'key_tag_braga_pf_nv'}
+     key_tag_oh = {'Key': 'Name', 'Value': 'key_tag_braga_pf_oh'}
 
      # -> security-groups
-     security_group_tag = {'Key': 'Name', 'Value': 'security_group_braga_pf'}
+     security_group_tag = {'Key': 'Name', 'Value': 'security_group_tag_braga_pf'}
 
      # -> instances
-     instance_tag_nv = {'Key': 'Name', 'Value': 'instance_braga_pf_nv'}
-     instance_tag_oh = {'Key': 'Name', 'Value': 'instance_braga_pf_oh'}
+     instance_tag_nv = {'Key': 'Name', 'Value': 'instance_tag_braga_pf_nv'}
+     instance_tag_oh = {'Key': 'Name', 'Value': 'instance_tag_braga_pf_oh'}
 
-     # -> script
+     # -> load_balancer
+     load_balancer_tag_nv = {'Key': 'Name', 'Value': 'load_balancer_tag_braga_pf_nv'}
+
+     # -> target_groups
+     target_group_tag_nv = {'Key': 'Name', 'Value': 'target_group_tag_braga_pf_nv'}
+
+     # -> autoscaling
+     autoscaling_tag_nv = {'Key': 'Name', 'Value': 'autoscaling_tag_braga_pf_nv'}
+
+     # script
      postgress_script = '''#!/bin/bash
      sudo apt update
      sudo apt install postgresql postgresql-contrib -y
@@ -251,7 +291,7 @@ def run_application() -> int:
      #aws_connection.create_security_group(security_group_name=security_group_name_oh, permissions=intern_security_group_oh)
      #postgres_ip, instance_id = aws_connection.create_instance(image_id=image_id_oh, user_data=postgress_script)
 
-     # -> script
+     # script
      django_script = '''#!/bin/bash
      sudo apt update
      git clone https://github.com/Gustavobb/tasks.git && mv tasks /home/ubuntu
